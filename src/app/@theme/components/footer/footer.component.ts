4c2f5c9ca99d43a1dfde4dcc3cf185f2c39ae559
developersHCS <developers@hcswifi.com.br>
2018-02-20 13:42:29 -0300

    Teste com o DS

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by"><b><a href="https://wwww.hcswifi.com.br" target="_blank">HCS-Wifi</a></b> 2018</span>
+  `,
+})
+export class FooterComponent {
+}
---
+++ src/app/pages/charts/d3/d3-advanced-pie.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-d3-advanced-pie',
+  template: `
+    <ngx-charts-advanced-pie-chart
+      [scheme]="colorScheme"
+      [results]="single">
+    </ngx-charts-advanced-pie-chart>
+  `,
+})
+export class D3AdvancedPieComponent implements OnDestroy {
+  single = [
+    {
+      name: 'Formulario',
+      value: 50,
+    },
+    {
+      name: 'Facebook',
+      value: 200,
+    },
+    {
+      name: 'France',
+      value: 10,
+    },
+  ];
+  colorScheme: any;
+  themeSubscription: any;
+
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+      const colors: any = config.variables;
+      this.colorScheme = {
+        domain: [colors.primaryLight, colors.infoLight, colors.successLight, colors.warningLight, colors.dangerLight],
+      };
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
+}
